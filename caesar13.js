92d3d2620172f6c07eea6f1515b5c48db7ddc1f7
Boris Yelnikoff <b.zawada84@gmail.com>
2021-05-10 10:25:03 +0200

    Refactor caesar13 module.
    Introduce CharEncoder class with value and range properties.
    Remove try...catch block in caesar13 function.
    Add validation for empty string in caesar13, encodeChar and
    CharEncoder's constructor.

--- caesar13.js
+++ caesar13.js
@@ -1,48 +1,74 @@
+class CharEncoder {
+  constructor(char) {
+    if (!char || typeof char !== "string") {
+      throw new Error(
+        "Invalid input argument. Non-empty character is expected."
+      );
+    }
+
+    //Value property
+    let value = char.charCodeAt(0);
+    Object.defineProperty(this, "value", {
+      get() {
+        return value;
+      },
+      set(newValue) {
+        if (newValue && typeof newValue === "number") {
+          value = newValue;
+        }
+      },
+    });
+
+    //Range property
+    const lowerCaseRange = { min: 97, max: 122 };
+    const upperCaseRange = { min: 65, max: 90 };
+    let range = null;
+    if (this.isInRange(lowerCaseRange)) {
+      range = lowerCaseRange;
+    } else if (this.isInRange(upperCaseRange)) {
+      range = upperCaseRange;
+    }
+    Object.defineProperty(this, "range", {
+      get() {
+        return range;
+      },
+    });
+  }
+
+  //methods
+  isInRange(range) {
+    if (range && range.min && range.max) {
+      return this.value >= range.min && this.value <= range.max;
+    }
+    return false;
+  }
+
+  encode(shift) {
+    if (!this.range) return this.value;
+
+    const rangeWidth = this.range.max - this.range.min + 1;
+    return (
+      ((this.value + shift - this.range.min) % rangeWidth) + this.range.min
+    );
+  }
+}
+
 function encodeChar(char, shift = 0) {
   if (!char || typeof char !== "string") {
     throw new Error("Invalid input argument. Non-empty character is expected.");
   }
 
-  const lowerCaseRange = { min: 97, max: 122 };
-  const upperCaseRange = { min: 65, max: 90 };
-  const encodedChar = {
-    value: char.charCodeAt(0),
-    isInRange(range) {
-      if (range && range.min && range.max) {
-        return this.value >= range.min && this.value <= range.max;
-      }
-      return false;
-    },
-    encodeValue(shift) {
-      if (!this.range) return this.value;
-
-      const rangeWidth = this.range.max - this.range.min + 1;
-      return (
-        ((this.value + shift - this.range.min) % rangeWidth) + this.range.min
-      );
-    },
-  };
-  encodedChar.range = encodedChar.isInRange(lowerCaseRange)
-    ? lowerCaseRange
-    : encodedChar.isInRange(upperCaseRange)
-    ? upperCaseRange
-    : null;
-
-  return String.fromCharCode(encodedChar.encodeValue(shift));
+  const charEncoder = new CharEncoder(char);
+  return String.fromCharCode(charEncoder.encode(shift));
 }
 
-function caesar13(input = "") {
-  if (typeof input !== "string") {
-    throw new Error("Invalid input argument. String is expected");
+function caesar13(input) {
+  if (!input || typeof input !== "string") {
+    throw new Error("Invalid input argument. Non-empty string is expected");
   }
 
   const shift = 13;
-  try {
-    return [...input].map((char) => encodeChar(char, shift)).join("");
-  } catch (error) {
-    console.log("Empty string passed as an argument");
-    return input;
-  }
+  return [...input].map((char) => encodeChar(char, shift)).join("");
 }
 
 export { caesar13 };
--- index.js
+++ index.js
@@ -11,5 +11,5 @@ function verify(input, goal) {
 
 verify(caesar13("Abcde"), "Nopqr");
 verify(caesar13("Tata"), "Gngn");
-verify(caesar13(""), "");
 verify(caesar13("PRZEPROGRAMOWANI"), "CEMRCEBTENZBJNAV");
+verify(caesar13("!PRZEPROGRAMOWANI6@#"), "!CEMRCEBTENZBJNAV6@#");
